70029b8dca7d92506a837d5373b71439e7fe0295
mftver <m.vermulst@student.avans.nl>
2021-06-16 16:17:30 +0200

    added enum

---
+++ models/Color.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class Color(Enum):
+    RED = 1
+    BLUE = 2
+    GREEN = 3
+    ORANGE = 4
+    PINK = 5
+    PURPLE = 6
+    YELLOW = 7
+    BLACK = 8
+    BROWN = 9
+    TURQUOISE = 10
--- src/mastermind.py
+++ src/mastermind.py
@@ -1,11 +1,11 @@
 import random
+from models.Color import Color
 
 
 class MasterMind:
     number_of_colors_in_code = 4
     duplicate_colors = False
     secret_code = []
-    possible_colors = ["Rood", "Blauw", "Groen", "Oranje", "Roze", "Paars", "Geel", "Zwart", "Bruin", "Turquoise"]
 
     def __init__(self, number_of_colors_in_code, duplicate_colors):
         self.number_of_colors_in_code = number_of_colors_in_code
@@ -15,11 +15,11 @@ class MasterMind:
         for color in range(self.amountOfColorsInCode):
             color = self.get_random_color()
 
-            if self.duplicate_colors:
+            if not self.duplicate_colors:
                 while color in self.secret_code:
                     color = self.get_random_color()
 
             self.secret_code.append()
 
     def get_random_color(self):
-        return self.possible_colors[random.randint(0, self.number_of_colors_in_code - 1)]
+        return self.Color(random.randint(0, self.number_of_colors_in_code - 1))
